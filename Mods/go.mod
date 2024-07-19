0e2c89e44eb6fc138e05378a5241ca4a3511e137
Divak-ar <144791644+Divak-ar@users.noreply.github.com>
2024-07-20 04:54:54 +0530

    working with go mod files

---
+++ Mods/go.mod
@@ -0,0 +1,3 @@
+module github.com/Divak-ar/gomodules
+
+go 1.22.5
---
+++ Mods/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello we will deep dive with mod files in GO")
+
+	// In terminal : go mod init github.com/Divak-ar/gomodules then go mod tidy
+
+}
